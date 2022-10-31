c940a035e9f21f61f431f886106b850e983381e6
GabrielePagnottae <gabriele.pagnotta1998@gmail.com>
2022-10-31 13:04:29 +0100

    fine esercizio

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,10 @@
+// -VARIABILI-
 let grid = document.getElementById('main');
-let x = 0
+let x = 0;
 
-console.log(grid)
 
-function squares(){
+// -FUNZIONI-
+function squares() {
     let div = document.createElement('div');
     div.classList.add('quadrato');
     let num = div.innerText = x += 1
@@ -11,20 +12,83 @@ function squares(){
     return div;
 }
 
-btn.addEventListener('click', function(){
+function squares_1() {
+    let div = document.createElement('div');
+    div.classList.add('quadrato_int');
+    let num = div.innerText = x += 1
 
-    for (let i = 0; i < 100; i++) {
-    
-        let quadrati = squares();
-    
-       quadrati.addEventListener('click', function(){
-            console.log(this);
-            this.classList.toggle('background');
-        })
-       
-        
-        grid.append(quadrati) 
+    return div;
+}
+
+function squares_2() {
+    let div = document.createElement('div');
+    div.classList.add('quadrato_dif');
+    let num = div.innerText = x += 1
+
+    return div;
+}
+
+
+
+// -EVENTO-
+btn.addEventListener('click', function () {
+    let level = document.getElementById('difficulty').value;
+
+
+    if (level == "facile") {
+
+        for (let i = 0; i < 100; i++) {
+
+            let quadrati = squares();
+
+                quadrati.addEventListener('click', function () {
+                this.classList.toggle('background');
+            })
+
+
+            grid.append(quadrati)
+        }
+
+
+    }
+
+    else if (level == "intermedio") {
+
+
+        for (let i = 0; i < 81; i++) {
+
+            let quadrati = squares_1();
+
+                quadrati.addEventListener('click', function () {
+                this.classList.toggle('background');
+            })
+
+
+            grid.append(quadrati)
+        }
+
+
+
+
+    }
+
+
+    else {
+        for (let i = 0; i < 49; i++) {
+
+            let quadrati = squares_2();
+
+                quadrati.addEventListener('click', function () {
+                this.classList.toggle('background');
+            })
+
+
+            grid.append(quadrati)
+        }
+
     }
 
 })
+
+
 
